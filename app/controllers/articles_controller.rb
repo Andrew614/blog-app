1c97d2f43099a2fd0136800aa267f15bd65d7d24
andrew614 <azhang213@gmail.com>
2019-10-02 02:13:58 -0400

    add CRUD operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,49 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        # @article = Article.new
+        # @article.title = params[:article][:title]
+        # @article.body = params[:article][:body]
+
+        # @article = Article.new(
+        #     title: params[:article][:title],
+        #     body: params[:article][:body])
+        @article = Article.new(article_params)
+        @article.save
+        flash.notice = "Article '#{@article.title}' has been created"
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id]).destroy
+        flash.notice = "Article '#{@article.title}' has been deleted"
+        redirect_to articles_path
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' updated!"
+
+        redirect_to article_path(@article)
     end
     
 end
